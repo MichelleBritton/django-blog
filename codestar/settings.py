2d356d866bb28a477d18482422269a2e9671dc3b
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-11-11 13:16:46 +0000

    change allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-mb.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-mb-6f5a219dd6af.herokuapp.com', 'localhost']
 
 
 # Application definition
