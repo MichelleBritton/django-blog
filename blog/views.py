d2e60d384b92c2d21ce62464799f7f4290451f16
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-11-15 20:50:36 +0000

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,65 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
-# This is using Django's generic view
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
 
-# This isn't using Django's generic view
-# When we created function based views in the Hello Django project, we pass the request object as an argument to the view, you can
-# check if the request methord was get or post so that you could decide what you wanted to do.  Class based views are different. Instead
-# of using an if statement to check the request method, we simply create class methods called get or post
+
 class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-        
+
         return render(
             request,
             "post_detail.html",
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
-            }
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
         )
+    
+    def post(self, request, slug, *args, **kwargs):
 
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
