d6a9b284b4c3d6ae4c025bbe1878e1bbf9eca454
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-11-15 16:17:02 +0000

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    # The first slug is a path converter which converts this text into a slug field, it tells django to match any slug string,
+    #  the second slug is a keyword name. The slug keyword name matches the 'slug parameter in the get method of the PostDetail 
+    # class in the blog/views.py file, that's how we link them together
+    path('<slug:slug/>', views.PostDetail.as_view(), name='post_detail'),
 ]
